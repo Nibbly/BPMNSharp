350224a7dd17136e17cec0b52a8081554073cd02
fabian77 <fabian77@web.de>
2019-01-27 20:17:26 +0100

    Corrects the location relative to point 0,0

--- BPMNSharp/Program.cs
+++ BPMNSharp/Program.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 using BPMN;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+using System.IO;
 
 namespace ReferenceVsValue
 {
@@ -13,6 +16,12 @@ namespace ReferenceVsValue
         static void Main(string[] args)
         {
             var model = Model.Read(@"C:\Users\Fabian\Desktop\Coding Projects\BPMNSharp\Diagram\diagram.bpmn");
+
+            var minX = ReturnMinValue(model, Coordinate.X);
+            var minY = ReturnMinValue(model, Coordinate.Y);
+
+            var moveX = -minX + 10;
+            var moveY = -minY + 10;
 
             for (int h = 0; h < model.Diagrams.Count; h++)
             {
@@ -29,17 +38,64 @@ namespace ReferenceVsValue
                         for (int j = 0; j < currentShape.Bounds.Count; j++)
                         {
                             var tempRec = currentShape.Bounds[j];
-                            tempRec.X = 15;
+                            tempRec.X += moveX;
+                            tempRec.Y += moveY;
                             currentShape.Bounds[j] = tempRec;
                         }
                     }
                 }
             }
 
-            //DisplayRecList(recList);
+            Image img = model.GetImage(0, 2.0f);
+            img.Save("diaNew.png", ImageFormat.Png);
 
+            var filePath = @"C:\Users\Fabian\Desktop\Coding Projects\BPMNSharp\BPMNSharp\bin\Debug";
+            bool pathOk = File.Exists(filePath);
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                Arguments = filePath,
+                FileName = "diaNew.png"
+            };
+
+            Process.Start(startInfo);
 
             Console.ReadLine();
+        }
+
+        public static int ReturnMinValue(Model model, Coordinate coordinate)
+        {
+            var min = -9999;
+
+            var query = (from value in model.Diagrams
+                        .SelectMany(x => x.Planes)
+                        .SelectMany(y => y.Shapes)
+                        .SelectMany(z => z.Bounds)
+                         select value).ToList();
+
+            if (coordinate == Coordinate.X)
+            {
+                min = (from x in query.Select(x => x.X)
+                       select x).ToList().Min();
+
+                return min;
+            }
+
+            if (coordinate == Coordinate.Y)
+            {
+                min = (from y in query.Select(y => y.Y)
+                       select y).ToList().Min();
+
+                return min;
+            }
+
+            return min;
+        }
+
+        public enum Coordinate
+        {
+            X = 1,
+            Y = 2
         }
 
         public static List<Rectangle> ReturnRecList()
@@ -64,7 +120,7 @@ namespace ReferenceVsValue
                 Console.WriteLine($"Rec {i} (old): {recList[i].ToString()}\tHash: {recList[i].GetHashCode()}");
 
                 recList[i] = tempRec;
-            
+
                 Console.WriteLine($"Rec {i} (new): {recList[i].ToString()}\tHash: {recList[i].GetHashCode()}\n");
             }
         }
